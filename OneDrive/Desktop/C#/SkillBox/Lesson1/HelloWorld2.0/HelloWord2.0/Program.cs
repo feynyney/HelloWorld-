bcbdeb64a3151b2fdfc968a4f2c2dd57a8c5f14a
Dmytro Makohonchuk <feynyney@gmail.com>
2022-03-31 20:37:19 +0300

    first commit

---
+++ OneDrive/Desktop/C#/SkillBox/Lesson1/HelloWorld2.0/HelloWord2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using static System.Console;
+
+namespace HelloWord2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Hello ");
+            Write("World ");
+            Write("!!!");
+
+            Write("\n");
+            Write("**************");
+            Write("\n");
+
+            WriteLine("Hello");
+            WriteLine("world");
+            WriteLine("!!!");
+
+            ReadLine();
+        }
+    }
+}
